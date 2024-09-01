eb6a5dc2ad2fdaac32d421df3065dbae57f141c7
hasithajayasundara <hasithajayasundara1@gmail.com>
2024-09-01 21:03:01 +0200

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
 import "./globals.css";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ConvexClientProvider } from "../components/convex-client-provider";
 
 export const metadata: Metadata = {
   title: "Collabo",
