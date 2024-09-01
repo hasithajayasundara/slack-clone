560754f3b4a69f0350eebf81aecb72e91ad41444
hasithajayasundara <hasithajayasundara1@gmail.com>
2024-09-01 21:16:50 +0200

    add sign out to profile drop down

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -4,4 +4,6 @@ import { api } from "@/convex/_generated/api";
 
 export const useCurrentUser = () => {
   const data = useQuery(api.users.current);
+  const isLoading = data === undefined;
+  return { data, isLoading };
 };
