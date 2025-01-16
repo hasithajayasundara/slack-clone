ad4dc294583b21b6800a1f8cdd5b07328341b3fa
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-16 10:09:47 +0100

    add create reactions endpoint

---
+++ hooks/use-reactions.ts
@@ -0,0 +1,64 @@
+import { useCallback, useMemo, useState } from 'react';
+
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { useMutation } from 'convex/react';
+
+type CreateRequest = {
+  value: string,
+  messageId: Id<"messages">,
+};
+
+type CreateResponse = Id<"reactions"> | null;
+
+type Options = {
+  onSuccess?: (data: CreateResponse) => void;
+  onError?: (e: Error) => void;
+  onSettled?: () => void;
+  throwError?: boolean;
+};
+
+export const useCreateReaction = () => {
+  const [data, setData] = useState<CreateResponse>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [state, setState] = useState<"success" | "error" | "settled" | "pending" | "idle">("idle");
+
+  const isPending = useMemo(() => state === "pending", [state]);
+  const isError = useMemo(() => state === "error", [state]);
+  const isSettled = useMemo(() => state === "settled", [state]);
+  const isSuccess = useMemo(() => state === "success", [state]);
+
+  const mutation = useMutation(api.reactions.toggle);
+
+  const mutate = useCallback(async (values: CreateRequest, options?: Options) => {
+    try {
+      setData(null);
+      setError(null);
+      setState("pending");
+      const response = await mutation(values);
+      options?.onSuccess?.(response);
+      setData(response);
+      setState("success");
+      return response;
+    } catch (err) {
+      options?.onError?.(err as Error);
+      if (options?.throwError) {
+        throw err;
+      }
+      setError(err as Error);
+      setState("error");
+    } finally {
+      options?.onSettled?.();
+    }
+  }, [mutation]);
+
+  return {
+    mutate,
+    data,
+    error,
+    isError,
+    isSettled,
+    isSuccess,
+    isPending,
+  };
+};
