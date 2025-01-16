2222d8d58f5eed80d7dfe14a54a90eda868f531b
hasithajayasundara <hasithajayasundara1@gmail.com>
2025-01-16 19:01:05 +0100

    add global search component

--- app/workspace/[workspaceId]/toolbar.tsx
+++ app/workspace/[workspaceId]/toolbar.tsx
@@ -1,21 +1,68 @@
 import { Info, Search } from "lucide-react";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { useGetWorkspace, useWorkspaceId } from "@/hooks";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator
+} from "@/components/ui/command";
+import { useGetChannels, useGetMembers, useGetWorkspace, useWorkspaceId } from "@/hooks";
 
 export const Toolbar = () => {
   const workspaceId = useWorkspaceId();
   const { data } = useGetWorkspace({ id: workspaceId });
+  const [open, setOpen] = useState(false);
+
+  const router = useRouter();
+
+  const { data: channels } = useGetChannels({ workspaceId });
+  const { data: members } = useGetMembers({ workspaceId });
+
+  const onSelect = (slug: string) => {
+    setOpen(false);
+    router.push(slug);
+  };
+
   return (
     <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
       <div className="flex-1"></div>
       <div className="min-w-[280px] max-[642px] grow-[2] shrink">
-        <Button size="sm" className="bg-accent/25 hover:bg-accent/25 w-full justify-start h-7 px-2">
+        <Button onClick={() => setOpen(true)} size="sm" className="bg-accent/25 hover:bg-accent/25 w-full justify-start h-7 px-2">
           <Search className="size-4 text-white mr-2" />
           <span className="text-white text-xs">
             Search {data?.name}
           </span>
         </Button>
+        <CommandDialog open={open} onOpenChange={setOpen}>
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Channels">
+              {channels?.map((channel) => (
+                <CommandItem
+                onSelect={() => onSelect(`/workspace/${workspaceId}/channel/${channel._id}`)}
+                >
+                  {channel.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Members">
+              {members?.map((member) => (
+                <CommandItem
+                  onSelect={() => onSelect(`/workspace/${workspaceId}/member/${member._id}`)}                >
+                  {member.user.name ?? ''}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </CommandDialog>
       </div>
       <div className="ml-auto flex-1 flex items-center justify-end">
         <Button variant="transparent" size="iconSm">
